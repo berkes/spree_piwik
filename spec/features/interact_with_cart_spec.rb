909aa9b92f5b3ca684a553a3e10613301931b339
Bèr Kessels <ber@berk.es>
2015-02-22 12:42:26 +0100

    Prefer double-quoted strings.

--- spec/features/interact_with_cart_spec.rb
+++ spec/features/interact_with_cart_spec.rb
@@ -1,32 +1,32 @@
-require 'spec_helper'
+require "spec_helper"
 
 feature "InteractWithCart", type: :feature do
   scenario "An anonymous user adds a product to the cart" do
-    @product = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    @product = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt")
     visit "products/#{@product.slug}"
     click_button "Add To Cart"
 
     tag_component = PiwikTagComponent.new
-    expect(tag_component.as_ecma).to eq fixture('one_product_in_cart.js')
+    expect(tag_component.as_ecma).to eq fixture("one_product_in_cart.js")
   end
 
   scenario "An anonymous user adds multiple products to the cart" do
-    @shirt = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    @shirt = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt")
     visit "products/#{@shirt.slug}"
     click_button "Add To Cart"
-    @sticker = FactoryGirl.create(:product, sku: 'SKU-2', name: 'RoR Sticker', price: 0.99)
+    @sticker = FactoryGirl.create(:product, sku: "SKU-2", name: "RoR Sticker", price: 0.99)
     visit "products/#{@sticker.slug}"
     click_button "Add To Cart"
 
     tag_component = PiwikTagComponent.new
-    expect(tag_component.as_ecma).to eq fixture('multiple_products_in_cart.js')
+    expect(tag_component.as_ecma).to eq fixture("multiple_products_in_cart.js")
   end
 
   scenario "An anonymous user removes a product from cart" do
-    @shirt = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    @shirt = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt")
     visit "products/#{@shirt.slug}"
     click_button "Add To Cart"
-    @sticker = FactoryGirl.create(:product, sku: 'SKU-2', name: 'RoR Sticker', price: 0.99)
+    @sticker = FactoryGirl.create(:product, sku: "SKU-2", name: "RoR Sticker", price: 0.99)
     visit "products/#{@sticker.slug}"
     click_button "Add To Cart"
 
@@ -34,16 +34,16 @@ feature "InteractWithCart", type: :feature do
     click_button "Update"
 
     tag_component = PiwikTagComponent.new
-    expect(tag_component.as_ecma).to eq fixture('one_product_in_cart.js')
+    expect(tag_component.as_ecma).to eq fixture("one_product_in_cart.js")
   end
 
   scenario "An anonymous user empties a cart" do
-    @shirt = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    @shirt = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt")
     visit "products/#{@shirt.slug}"
     click_button "Add To Cart"
     click_button "Empty Cart"
 
     tag_component = PiwikTagComponent.new
-    expect(tag_component.as_ecma).to eq fixture('empty_cart.js')
+    expect(tag_component.as_ecma).to eq fixture("empty_cart.js")
   end
 end
