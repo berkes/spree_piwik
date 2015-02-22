57d7de4facae7ea5692ba8a0eeea095c88c9fbee
Bèr Kessels <ber@berk.es>
2015-02-22 13:05:12 +0100

    Prefer double-quoted strings.

--- spec/features/visit_product_page_spec.rb
+++ spec/features/visit_product_page_spec.rb
@@ -1,11 +1,11 @@
-require 'spec_helper'
+require "spec_helper"
 
 feature "TrackingTagForProduct", type: :feature do
   # As a shop-owner
-  # I want to send stats with the product's attributes to piwik
+  # I want to send stats with the product"s attributes to piwik
   # So that I can allow Piwik to process reports on SKU and products.
   scenario "An anonymous user is tracked on a product page" do
-    @product = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    @product = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt")
     # TODO replace with visit "product/slug"
     visit "/"
     click_link(@product.name)
@@ -15,20 +15,20 @@ feature "TrackingTagForProduct", type: :feature do
 
     tag_component = PiwikTagComponent.new
     expect(tag_component).to be_in_html
-    expect(tag_component.to_s).to include 'setEcommerceView'
-    expect(tag_component.as_ecma).to eq fixture('product_example_com.js')
+    expect(tag_component.to_s).to include "setEcommerceView"
+    expect(tag_component.as_ecma).to eq fixture("product_example_com.js")
   end
 
   # As a shop-owner
   # I want to send the categories in a product to piwik
   # So that can filter stats in Piwik on categories
-  scenario 'An anonymous user is tracked on a product page with categories' do
+  scenario "An anonymous user is tracked on a product page with categories" do
     taxons = [
       FactoryGirl.create(:taxon, name: "shirts"),
       FactoryGirl.create(:taxon, name: "ror"),
       FactoryGirl.create(:taxon, name: "webdev"),
     ]
-    @product = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt', taxons: taxons)
+    @product = FactoryGirl.create(:product, sku: "SKU-1", name: "RoR Shirt", taxons: taxons)
 
     # TODO replace with visit "product/slug"
     visit "/"
@@ -39,7 +39,7 @@ feature "TrackingTagForProduct", type: :feature do
 
     tag_component = PiwikTagComponent.new
     expect(tag_component).to be_in_html
-    expect(tag_component.to_s).to include 'setEcommerceView'
-    expect(tag_component.as_ecma).to eq fixture('product_example_com_categories.js')
+    expect(tag_component.to_s).to include "setEcommerceView"
+    expect(tag_component.as_ecma).to eq fixture("product_example_com_categories.js")
   end
 end
