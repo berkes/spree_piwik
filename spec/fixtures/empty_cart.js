3bd17a4679d921c581d848f563afa74c9dbb0850
Bèr Kessels <ber@berk.es>
2015-02-22 13:09:26 +0100

    Don't mix single and double quotes.

--- spec/fixtures/empty_cart.js
+++ spec/fixtures/empty_cart.js
@@ -1,5 +1,5 @@
 var _paq = _paq || [];
-(function(){ var u=(("https:" == document.location.protocol) ? "https://example.com/" : "http://example.com/");
+(function(){ var u=(('https:' == document.location.protocol) ? 'https://example.com/' : 'http://example.com/');
 _paq.push(['setSiteId', 0]);
 _paq.push(['setTrackerUrl', u+'piwik.php']);
 _paq.push(['trackEcommerceCartUpdate', 0.0]);
