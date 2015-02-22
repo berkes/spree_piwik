b67bf0908074cb6684315c6a421c4f6884147491
Bèr Kessels <ber@berk.es>
2015-02-22 11:49:39 +0100

    Add feature to track cart removals and updates

--- spec/features/interact_with_cart_spec.rb
+++ spec/features/interact_with_cart_spec.rb
@@ -7,7 +7,7 @@ feature "InteractWithCart", type: :feature do
     click_button "Add To Cart"
 
     tag_component = PiwikTagComponent.new
-    expect(tag_component.as_ecma).to eq fixture('product_added_to_cart.js')
+    expect(tag_component.as_ecma).to eq fixture('one_product_in_cart.js')
   end
 
   scenario "An anonymous user adds multiple products to the cart" do
@@ -20,5 +20,20 @@ feature "InteractWithCart", type: :feature do
 
     tag_component = PiwikTagComponent.new
     expect(tag_component.as_ecma).to eq fixture('multiple_products_in_cart.js')
+  end
+
+  scenario "An anonymous user removes a product from cart" do
+    @shirt = FactoryGirl.create(:product, sku: 'SKU-1', name: 'RoR Shirt')
+    visit "products/#{@shirt.slug}"
+    click_button "Add To Cart"
+    @sticker = FactoryGirl.create(:product, sku: 'SKU-2', name: 'RoR Sticker', price: 0.99)
+    visit "products/#{@sticker.slug}"
+    click_button "Add To Cart"
+
+    fill_in(:order_line_items_attributes_1_quantity, with: 0)
+    click_button "Update"
+
+    tag_component = PiwikTagComponent.new
+    expect(tag_component.as_ecma).to eq fixture('one_product_in_cart.js')
   end
 end
