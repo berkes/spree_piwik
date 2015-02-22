49572a013d7ef9c77f10dd47b9ebe455b6c8c160
Bèr Kessels <ber@berk.es>
2015-02-22 13:02:12 +0100

    Prefer double-quoted strings.

--- spec/features/visit_home_pages_spec.rb
+++ spec/features/visit_home_pages_spec.rb
@@ -1,9 +1,9 @@
-require 'spec_helper'
+require "spec_helper"
 
 feature "TrackingTagForIndex", type: :feature do
   before do
     # User has set piwik_url and piwik_id
-    Spree::Piwik::Config.set(piwik_url: 'piwik.example.com')
+    Spree::Piwik::Config.set(piwik_url: "piwik.example.com")
     Spree::Piwik::Config.set(piwik_id: 1337)
   end
 
@@ -15,7 +15,7 @@ feature "TrackingTagForIndex", type: :feature do
 
     tag_component = PiwikTagComponent.new
     expect(tag_component).to be_in_html
-    expect(tag_component.tracker_url_assigment).to include('piwik.example.com')
-    expect(tag_component.as_ecma).to eq fixture('index_example_com.js')
+    expect(tag_component.tracker_url_assigment).to include("piwik.example.com")
+    expect(tag_component.as_ecma).to eq fixture("index_example_com.js")
   end
 end
