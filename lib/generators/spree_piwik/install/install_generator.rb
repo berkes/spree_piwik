66bc95fb32d23f7fa133bd114239fa719e5f1134
Bèr Kessels <ber@berk.es>
2015-02-27 13:43:54 +0100

    Remove injecting of assets since we don't have assets.

--- lib/generators/spree_piwik/install/install_generator.rb
+++ lib/generators/spree_piwik/install/install_generator.rb
@@ -9,17 +9,6 @@ module SpreePiwik
       def copy_initializer
         template "spree_piwik.rb", "config/initializers/spree_piwik.rb"
       end
-
-      def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_piwik\n"
-        append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_piwik\n"
-      end
-
-      def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_piwik\n", :before => /\*\//, :verbose => true
-        inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_piwik\n", :before => /\*\//, :verbose => true
-      end
-
     end
   end
 end
