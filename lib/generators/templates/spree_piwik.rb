90b920f6d93d9b734d0cb0a129363a9fd7373705
Bèr Kessels <ber@berk.es>
2015-02-27 13:47:18 +0100

    Fix spacing issues (houndci)

--- lib/generators/templates/spree_piwik.rb
+++ lib/generators/templates/spree_piwik.rb
@@ -1,5 +1,4 @@
 # Configure Spree
 # Uncomment these lines and provide your own values.
-#Spree::Piwik::Config[:piwik_url] = "piwik.example.com"
-#Spree::Piwik::Config[:piwik_id] =  1337
-
+# Spree::Piwik::Config[:piwik_url] = "piwik.example.com"
+# Spree::Piwik::Config[:piwik_id] =  1337
