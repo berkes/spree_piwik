01baf2f1504ac5e2480a414b3a63632afe52e33c
Bèr Kessels <ber@berk.es>
2015-02-27 13:37:57 +0100

    Remove migration-tasks from install_generator.
    We don't have migrations.

--- lib/generators/spree_piwik/install/install_generator.rb
+++ lib/generators/spree_piwik/install/install_generator.rb
@@ -20,18 +20,6 @@ module SpreePiwik
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_piwik\n", :before => /\*\//, :verbose => true
       end
 
-      def add_migrations
-        run 'bundle exec rake railties:install:migrations FROM=spree_piwik'
-      end
-
-      def run_migrations
-        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
-        if run_migrations
-          run 'bundle exec rake db:migrate'
-        else
-          puts 'Skipping rake db:migrate, don\'t forget to run it!'
-        end
-      end
     end
   end
 end
